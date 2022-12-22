31ddbf7b3e72c59dd6a66bd1b7c004e8b07384d7
ceciliacaporale <cecicaporale16@gmail.com>
2022-12-22 15:43:07 -0300

    Inicializando JS

--- main.js
+++ main.js
@@ -3,5 +3,17 @@ function tocaSomPom () {
 
 }
 
-document.querySelector('.tecla_pom');
 
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+//enquanto
+while (contador < 9) {
+    listaDeTeclas[contador].onclick = tocaSomPom;
+
+    contador = contador + 1;
+
+    console.log(contador);
+}
